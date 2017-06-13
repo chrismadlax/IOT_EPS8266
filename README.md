6ad972ff9d8f699a9e57477b1216cd112def6e45
chrismadlax <chrismadlax@gmail.com>
2017-06-14 01:06:53 +0800

    update problem fix infomation in README.md and "test.py" for first test

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ for Maker's Attic 創客閣樓
 _PCB: Eagle PCB project 
 
 _CODE: uPy source code 
+
+problem fix
+	transistor T1 & T2 pin E/B must swap
+	crystal can be remove
+	resistance R19 can be short to GND
---
+++ _Code/test.py
@@ -0,0 +1,4 @@
+from machine import Pinimport time
+print('hello world')
+pin = Pin(10, Pin.OUT)
+for i in range(10):	pin.low()	time.sleep(0.5)	pin.high()	time.sleep(0.5)pin.high()while True:	if pin.value() == 0:		print('USR buttom push!!')	time.sleep(0.1)
