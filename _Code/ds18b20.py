452ec70b901f11fc8bee2b250fcb1fbbe8a6cbab
chris <chris@prowavegroup.com>
2017-06-29 18:53:23 +0800

    edit ds18b20.py runt is one time

--- _Code/ds18b20.py
+++ _Code/ds18b20.py
@@ -1,14 +1,13 @@
 from machine import Pin
 import onewire
 
-ow = onewire.OneWire(Pin(12))
+ow = onewire.OneWire(Pin(4))
 
 import time, ds18x20
 
 ds = ds18x20.DS18X20(ow)
-while True:
-	roms = ds.scan()
-	ds.convert_temp()
-	time.sleep_ms(750)
-	for rom in roms:
-		print(ds.read_temp(rom)) 
+roms = ds.scan()
+ds.convert_temp()
+time.sleep_ms(750)
+for rom in roms:
+	print(ds.read_temp(rom)) 
